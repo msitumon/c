bac65af7feff6dd194af96039239deccef1190b6
Samiul Islam Tumon <msitumon2@gmail.com>
2026-02-24 06:01:01 +0600

    oddNumbers

---
+++ regularPractice/odd_Numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int x,i;
+    printf("Enter the value of x: ");
+    scanf("%d",&x);
+    for(i=0; i<=x; i++){
+        if(i%2!=0){
+            printf("%d\n",i);
+        }
+    }
+return 0;
+}
