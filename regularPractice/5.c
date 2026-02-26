e1685b864abff38040924d3e241fb69bf1ad7616
Samiul Islam Tumon <msitumon2@gmail.com>
2026-02-26 22:57:16 +0600

    Error finding

---
+++ regularPractice/5.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    char grade='A';
+    int mark=85;
+    float percentage=mark/10.0;
+    printf("Percentage: %f\n",percentage);
+return 0;
+}
---
+++ regularPractice/6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int i,a=10;
+    float b=5.5;
+    if(a==10){
+        printf("Equal\n");
+    }
+    printf("b=%f\n",b);
+    for(i=0; i<3; i++){
+        printf("%d\n",i);
+    }
+return 0;
+}
