f11a818c9ae1b45b986aa9704e31144e241ce1fb
Samiul Islam Tumon <msitumon2@gmail.com>
2026-02-28 22:31:40 +0600

    Temperature conversation between calvin and fahrenheit temperature

---
+++ regularPractice/temperature_calculation.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    float c,f;
+    printf("Enter calvin temperature: ");
+    scanf("%f",&c);
+    f=9*c/5+32;
+    printf("Fahrenheit temperature = %.2f\n",f);
+return 0;
+}
---
+++ regularPractice/temperature_calculation2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    float c,f;
+    printf("Enter fahrenheit temperature: ");
+    scanf("%f",&f);
+    c=(f-32)*5/9;
+    printf("Calvin temperature = %.2f\n",c);
+return 0;
+}
