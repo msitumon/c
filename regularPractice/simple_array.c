16379877061e92a2ce4abd107236846b0862d1e6
Samiul Islam Tumon <msitumon2@gmail.com>
2026-03-04 23:10:16 +0600

    Use of an array and it's output

---
+++ regularPractice/simple_array.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int numbers[]={10, 20, 30, 40, 50,60};
+    char grades[]={'A', 'B', 'C', 'D', 'F'};
+    char name[]="Samiul Islam";
+
+    //individual output of array
+    printf("%d\n",numbers[4]);
+    printf("%d\n",grades[1]);
+    printf("%s\n",name);
+return 0;
+}
