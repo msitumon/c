21397e145602b51df6ee409e8845bea0f353ce92
Samiul Islam Tumon <msitumon2@gmail.com>
2026-03-05 22:35:13 +0600

    Separate positive value from array.

---
+++ regularPractice/positive_num_array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int i,n,x,arr[n];
+    printf("Enter the size of array: ");
+    scanf("%d",&n);
+    for(i=0; i<n; i++){
+        x=i+1;
+        printf("Enter the %d no array value: ",x);
+        scanf("%d",&arr[i]);
+    }
+    printf("\n\nPositive value of the array: \n");
+    for(i=0; i<n; i++){
+        if(arr[i]>=0){
+            printf("%d\n",arr[i]);
+        }
+    }
+return 0;
+}
