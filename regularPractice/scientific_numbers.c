30fe46255ea9a00513830005884a835f4a240e9d
Samiul Islam Tumon <msitumon2@gmail.com>
2026-03-06 22:57:24 +0600

    Scientific values or large-scale data.

---
+++ regularPractice/scientific_numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int num=35e3;
+    float f1=101.05E5;
+    double d1=14.20135e2;
+
+    printf("%i\n",num);
+    printf("%F\n",f1);
+    printf("%lf\n",d1);
+return 0;
+}
