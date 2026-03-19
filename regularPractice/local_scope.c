2c2c847a3f2a66a70a41cf3e83f11527e92691a8
Samiul Islam Tumon <msitumon2@gmail.com>
2026-03-19 22:59:36 +0600

    Variables are only accessible inside the region they are created.

---
+++ regularPractice/local_scope.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void myFunction(){
+    //Local variable that belongs to myFunction
+    int x=22;
+
+    //Print the variable x
+    printf("%d",x);
+}
+int main(){
+    myFunction();
+    return 0;
+}
