546927decceea9fb3d660b08b64e0641a3a6d963
Samiul Islam Tumon <msitumon2@gmail.com>
2026-04-08 01:12:26 +0600

    Print arrays in reverse order.

---
+++ Course/Array/3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int i,j,k,n;
+    printf("Enter the n: ");
+    scanf("%d",&n );
+    int a[n],b[n];
+    j=n;
+    printf("Enter the element of the array: \n")
+    for(i=0; i<n; i++){
+        scanf("%d",&a[i]);
+        j--;
+        b[j]=a[i];
+    }
+    printf("Reverse of the element: \n");
+    for(k=0; k<n; k++){
+        printf("%d\n",b[k]);
+    }
+return 0;
+}
