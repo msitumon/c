876f7bf56af2ab84611fa1b7638e9f7cac15eec1
Samiul Islam Tumon <msitumon2@gmail.com>
2026-04-26 23:35:43 +0600

    Sort an array in ascending/descending order,

---
+++ Course/Array/6.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int i,j,n,temp,a[100];
+    scanf("%d",&n);
+    printf("Enter element: \n");
+    for(i=0; i<n; i++){
+        scanf("%d",&a[i]);
+    }
+    for(i=0; i<n-1; i++){
+        for(j=i+1; j<n; j++){
+            if(a[i]>a[j]){
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    printf("Acending order: \n");
+    for(i=0; i<n; i++){
+        printf("%d\n",a[i]);
+    }
+    printf("Decending order: \n");
+    for(i=n-1; i>=0; i--){
+        printf("%d\n",a[i]);
+    }
+return 0;
+}
